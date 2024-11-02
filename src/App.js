4264268a9db1fd57f150b171f7fce9b7417f849f
gawhar <gawharsalman@outlook.com>
2024-11-02 15:14:08 +0530

    deleted:    public/favicon.ico 	modified:   public/index.html 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	deleted:    public/manifest.json 	deleted:    public/robots.txt 	deleted:    src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	modified:   src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	new file:   src/profile.js 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { profiles } from "./profile";
+const App = () => {
+  const [index, setIndex] = useState(0);
+  const checkIndex = (num) => {
+    if (num > profiles.length - 1) {
+      return 0;
+    } else if (num < 0) {
+      return profiles.length - 1;
+    } else {
+      return num;
+    }
+  };
 
-function App() {
+  const nextBtnHandler = (num) => {
+    let newIndex = num + 1;
+    newIndex = checkIndex(newIndex);
+    setIndex(newIndex);
+  };
+
+  const previousBtnHandler = (num) => {
+    let newIndex = num - 1;
+    newIndex = checkIndex(newIndex);
+    setIndex(newIndex);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <div>
+        <Profile profile={profiles[index]} />
+        <div className="hr_container-3">
+          <button onClick={() => previousBtnHandler(index)}>
+            <span className="material-symbols-outlined">arrow_back_ios</span>
+          </button>
+          <button onClick={() => nextBtnHandler(index)}>
+            <span className="material-symbols-outlined">arrow_forward_ios</span>
+          </button>
+        </div>
+      </div>
+    </section>
   );
-}
+};
+
+const Profile = (props) => {
+  const { id, img, name, designation, summery } = props.profile;
+  return (
+    <>
+      <div className="hr_container-1">
+        <img src={img} alt={name}></img>
+        <p>{summery}</p>
+      </div>
+      <div className="hr_container-2">
+        <div>
+          {profiles.map((item) => {
+            return item.id === id ? (
+              <button className="active_profile" key={item.id}></button>
+            ) : (
+              <button key={item.id}></button>
+            );
+          })}
+        </div>
+        <div>
+          <h1>{name.toUpperCase()}</h1>
+          <h2>{designation.toUpperCase()}</h2>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default App;
---
+++ src/profile.js
@@ -0,0 +1,30 @@
+export const profiles = [
+  {
+    id: 1,
+    img: "https://www.course-api.com/images/people/person-1.jpeg",
+    name: "susan smith",
+    designation: "web developer",
+    summery: `I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry`,
+  },
+  {
+    id: 2,
+    img: "https://www.course-api.com/images/people/person-2.jpeg",
+    name: "anna johnson",
+    designation: "web designer",
+    summery: `Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.`,
+  },
+  {
+    id: 3,
+    img: "https://www.course-api.com/images/people/person-3.jpeg",
+    name: "bill anderson",
+    designation: "the boss",
+    summery: `Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. `,
+  },
+  {
+    id: 4,
+    img: "https://www.course-api.com/images/people/person-4.jpeg",
+    name: "peter jones",
+    designation: "intern",
+    summery: `Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.`,
+  },
+];
